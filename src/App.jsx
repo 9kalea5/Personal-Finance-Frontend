7550dab0d48ab2f15f579af95aeaa6b74ef1f8f2
kaleab <alebachewkaleab@gmail.com>
2025-05-31 17:45:13 +0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { ToastContainer } from 'react-toastify';
+import { theme } from './theme';
+import Register from './components/Register';
+import Login from './components/Login';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
- }
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </Router>
+    </ThemeProvider>
+  );
+}
 
-export default App
+export default App;
